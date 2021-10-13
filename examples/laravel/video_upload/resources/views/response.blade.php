014cb343b46358942f46e9a5809d0201580c3daf
Erikka <19295862+erikkai@users.noreply.github.com>
2021-10-13 15:43:09 -0700

    Add files via upload

---
+++ examples/laravel/video_upload/resources/views/response.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>Response Page</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+Success!
+</body>
+</html>
