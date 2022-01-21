6104e1f49f2a6f52ba282a24e65ae2a96d456919
Erikka <19295862+erikkai@users.noreply.github.com>
2022-01-20 20:49:37 -0800

    Create authenticate.php

---
+++ examples/authenticate.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ .'/vendor/autoload.php';
+
+use Symfony\Component\HttpClient\Psr18Client;
+use ApiVideo\Client\Client;
+use ApiVideo\Client\Model\VideoCreationPayload;
+
+$apiKey = 'your API key here';
+$apiVideoEndpoint = 'https://ws.api.video';
+
+$httpClient = new \Symfony\Component\HttpClient\Psr18Client();
+$client = new ApiVideo\Client\Client(
+    $apiVideoEndpoint,
+    $apiKey,
+    $httpClient
+);
+
+/* Your refresh token is handled for you */
