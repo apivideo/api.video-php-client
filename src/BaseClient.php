f24fd861325bfe3845083df6d7d1ba95896f5ee5
olivierapivideo <olivierapivideo@users.noreply.github.com>
2024-09-05 07:43:02 +0000

    Add sort parameters in analytics routes

--- src/BaseClient.php
+++ src/BaseClient.php
@@ -78,7 +78,7 @@ class BaseClient
         $this->originSdkHeaderValue = "";
 
         if ($apiKey) {
-            $this->authenticator = new Authenticator($this, $apiKey, 'php:1.4.0');
+            $this->authenticator = new Authenticator($this, $apiKey, 'php:1.4.1');
         }
     }
 
@@ -111,7 +111,7 @@ class BaseClient
         if($this->originSdkHeaderValue) {
             $request = $request->withHeader('AV-Origin-Sdk', $this->originSdkHeaderValue);
         }
-        $request = $request->withHeader('AV-Origin-Client', 'php:1.4.0');
+        $request = $request->withHeader('AV-Origin-Client', 'php:1.4.1');
 
         return $this->sendRequest($request, $skipAuthRequest);
     }
